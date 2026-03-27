05fc796198f36ea47b9b8c88e73b5a2dc22f14d8
psipac-2025347 <psipac-2025347@kinal.edu.gt>
2026-03-27 01:59:00 -0600

    feat: Se agregó IDetalleVenta y tambien corrigiendo errores en otros archivos

---
+++ src/main/java/com/pablosipac/kinalapp/service/IDetalleVentaService.java
@@ -0,0 +1,26 @@
+package com.pablosipac.kinalapp.service;
+
+import com.pablosipac.kinalapp.entity.DetalleVenta;
+import java.util.List;
+import java.util.Optional;
+
+public interface IDetalleVentaService {
+
+    List<DetalleVenta> listarTodos();
+
+    Optional<DetalleVenta> buscarPorCodigo(Long codigoDetalleVenta);
+
+    DetalleVenta guardar(DetalleVenta detalleVenta);
+
+    DetalleVenta actualizar(Long codigoDetalleVenta, DetalleVenta detalleVenta);
+
+    void eliminar(Long codigoDetalleVenta);
+
+    boolean existePorCodigo(Long codigoDetalleVenta);
+
+    List<DetalleVenta> listarPorEstado(Long estado);
+
+    List<DetalleVenta> buscarPorVenta(Long codigoVenta);
+
+    List<DetalleVenta> buscarPorProducto(Long codigoProducto);
+}

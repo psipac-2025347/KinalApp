8541ceea59f18fa33f14a803402dafd5e3460e17
psipac-2025347 <psipac-2025347@kinal.edu.gt>
2026-05-01 15:25:10 -0600

    feat: Se agregó la configuracion para SpringSecurity y que solo funcione con User y admin

--- src/main/java/com/pablosipac/kinalapp/repository/UsuarioRepository.java
+++ src/main/java/com/pablosipac/kinalapp/repository/UsuarioRepository.java
@@ -4,7 +4,10 @@ import com.pablosipac.kinalapp.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
     List<Usuario> findByEstado (Long Estado);
+
+    Optional<Usuario> findByUsername(String username);
 }

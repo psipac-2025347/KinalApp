1caa708061e13b64b0e1401db7e6eae67b4d688f
psipac-2025347 <psipac-2025347@kinal.edu.gt>
2026-05-12 15:24:48 -0600

    feat:Se agregaron cambios en usuario haciendo que usuario pueda agregar pero no actualizar ni eliminar

--- src/main/java/com/pablosipac/kinalapp/security/SecurityConfig.java
+++ src/main/java/com/pablosipac/kinalapp/security/SecurityConfig.java
@@ -26,32 +26,35 @@ public class SecurityConfig {
 
                         .requestMatchers("/css/**", "/js/**",
                                 "/vista/login", "/vista/registro").permitAll()
-
-                        .requestMatchers("/vista/clientes/nuevo").hasRole("ADMIN")
-                        .requestMatchers("/vista/clientes/guardar").hasRole("ADMIN")
+                        .requestMatchers("/vista/clientes/nuevo").permitAll()
+                        .requestMatchers("/vista/clientes/guardar").permitAll()
                         .requestMatchers("/vista/clientes/editar/**").hasRole("ADMIN")
                         .requestMatchers("/vista/clientes/actualizar/**").hasRole("ADMIN")
                         .requestMatchers("/vista/clientes/eliminar/**").hasRole("ADMIN")
 
-                        .requestMatchers("/vista/productos/nuevo").hasRole("ADMIN")
-                        .requestMatchers("/vista/productos/guardar").hasRole("ADMIN")
+                        .requestMatchers("/vista/productos/nuevo").permitAll()
+                        .requestMatchers("/vista/productos/guardar").permitAll()
                         .requestMatchers("/vista/productos/editar/**").hasRole("ADMIN")
                         .requestMatchers("/vista/productos/actualizar/**").hasRole("ADMIN")
                         .requestMatchers("/vista/productos/eliminar/**").hasRole("ADMIN")
 
-                        .requestMatchers("/vista/ventas/nuevo").hasRole("ADMIN")
-                        .requestMatchers("/vista/ventas/guardar").hasRole("ADMIN")
+                        .requestMatchers("/vista/ventas/nuevo").permitAll()
+                        .requestMatchers("/vista/ventas/guardar").permitAll()
                         .requestMatchers("/vista/ventas/editar/**").hasRole("ADMIN")
                         .requestMatchers("/vista/ventas/actualizar/**").hasRole("ADMIN")
                         .requestMatchers("/vista/ventas/eliminar/**").hasRole("ADMIN")
 
-                        .requestMatchers("/vista/detalles/nuevo").hasRole("ADMIN")
-                        .requestMatchers("/vista/detalles/guardar").hasRole("ADMIN")
+                        .requestMatchers("/vista/detalles/nuevo").permitAll()
+                        .requestMatchers("/vista/detalles/guardar").permitAll()
                         .requestMatchers("/vista/detalles/editar/**").hasRole("ADMIN")
                         .requestMatchers("/vista/detalles/actualizar/**").hasRole("ADMIN")
                         .requestMatchers("/vista/detalles/eliminar/**").hasRole("ADMIN")
 
-                        .requestMatchers("/vista/usuarios/**").hasRole("ADMIN")
+                        .requestMatchers("/vista/usuarios/nuevo").permitAll()
+                        .requestMatchers("/vista/usuarios/guardar").permitAll()
+                        .requestMatchers("/vista/usuarios/editar/**").hasRole("ADMIN")
+                        .requestMatchers("/vista/usuarios/actualizar/**").hasRole("ADMIN")
+                        .requestMatchers("/vista/usuarios/eliminar/**").hasRole("ADMIN")
 
                         .anyRequest().authenticated()
                 )
